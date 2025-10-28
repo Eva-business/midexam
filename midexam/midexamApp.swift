5d76d6f955f86fcc45b4938e7b126f9c471c13ae
GaryHu <garyhu17@gmail.com>
2025-10-28 15:30:24 +0800

    Initial Commit

---
+++ midexam/midexamApp.swift
@@ -0,0 +1,17 @@
+//
+//  midexamApp.swift
+//  midexam
+//
+//  Created by user13 on 2025/10/28.
+//
+
+import SwiftUI
+
+@main
+struct midexamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
